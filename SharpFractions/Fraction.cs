1c8bc8c7178e54389bbfa4d07c986d4baf3c20a7
joharasmus <joha.rasmus@gmail.com>
2022-06-23 15:25:53 +0200

    Moved an import

--- SharpFractions/Fraction.cs
+++ SharpFractions/Fraction.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace SharpMathLib;
+﻿namespace SharpMathLib;
 
 public readonly struct Fraction : IComparable<Fraction>, IEquatable<Fraction>
 {
--- SharpFractions/Imports.cs
+++ SharpFractions/Imports.cs
@@ -4,5 +4,6 @@ global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
 global using System.Numerics;
+global using System.Diagnostics.CodeAnalysis;
 
 namespace SharpMathLib;
